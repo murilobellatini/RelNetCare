20a6d8a921193489d24f8edcf63e7dc66463f67c
Murilo <43999445+murilobellatini@users.noreply.github.com>
2023-06-22 01:01:33 +0200

    Fix bugs

--- scripts/dialogre_to_neo4j.py
+++ scripts/dialogre_to_neo4j.py
@@ -24,12 +24,15 @@ def add_entity_to_dialogue(tx, dialogue_id, entity):
         MERGE (d)-[:CONTAINS]->(e)
         """, dialogue_id=dialogue_id, entity=entity)
 
-def add_relation(tx, entity1, entity2, relation):
+def add_relation(tx, dialogue_id, entity1, entity2, relation):
     tx.run("""
         MATCH (a:Entity {name: $entity1})
         MATCH (b:Entity {name: $entity2})
-        MERGE (a)-[:RELATION {type: $relation}]->(b)
-        """, entity1=entity1, entity2=entity2, relation=relation)
+        MERGE (a)-[r:RELATION {type: $relation}]->(b)
+        ON CREATE SET r.dialogue_id = [$dialogue_id]
+        ON MATCH SET r.dialogue_id = coalesce(r.dialogue_id + [$dialogue_id], [$dialogue_id])
+        """, dialogue_id=dialogue_id, entity1=entity1, entity2=entity2, relation=relation)
+
 
 def add_dataset(tx, dataset_name):
     tx.run("CREATE (:Dataset {name: $name})", name=dataset_name)
@@ -43,32 +46,42 @@ def add_dialogue_to_dataset(tx, dialogue_id, dataset_name):
 
 
 with driver.session() as session:
+    counter = 0
     for json_file in tqdm(json_files):
         dataset_name = json_file.split('.')[0]  # assuming the dataset name is the filename without extension
-        session.write_transaction(add_dataset, dataset_name)  # create a dataset node
+        session.execute_write(add_dataset, dataset_name)  # create a dataset node
         with open(dialogues_path / json_file, 'r', encoding='utf-8') as f:
             dialogues = json.load(f)
             for i, dialogue_data in tqdm(enumerate(dialogues)):
+                
+                idx = (i + counter)
                 dialogue, entities_relations = dialogue_data
 
                 # Add the dialogue to the graph and associate it with the dataset
-                session.write_transaction(add_dialogue, i, dialogue, dataset_name)
-                session.write_transaction(add_dialogue_to_dataset, i, dataset_name)
+                session.execute_write(add_dialogue, idx, dialogue, dataset_name)
+                session.execute_write(add_dialogue_to_dataset, idx, dataset_name)
 
                 for relation in entities_relations:
                     x = relation['x']
                     y = relation['y']
                     r = ', '.join(relation['r'])
-
+                    
+                    if "Speaker" in x:
+                        x = f"{idx}_{x}"
+                    if "Speaker" in y:
+                        y = f"{idx}_{y}"
+                        
                     # Add the entities to the graph
-                    session.write_transaction(add_entity, x)
-                    session.write_transaction(add_entity, y)
+                    session.execute_write(add_entity, x)
+                    session.execute_write(add_entity, y)
 
                     # Associate the entities with the dialogue
-                    session.write_transaction(add_entity_to_dialogue, i, x)
-                    session.write_transaction(add_entity_to_dialogue, i, y)
+                    session.execute_write(add_entity_to_dialogue, idx, x)
+                    session.execute_write(add_entity_to_dialogue, idx, y)
 
                     # Add the relationship to the graph
-                    session.write_transaction(add_relation, x, y, r)
+                    session.execute_write(add_relation, idx, x, y, r)
+        
+        counter += (i + 1)
 
 driver.close()
