8f91ec0778e18536e09ff9254c8616b7d1c038cf
Murilo <43999445+murilobellatini@users.noreply.github.com>
2023-06-22 13:19:24 +0200

    Fix folders

--- scripts/dialogre_to_neo4j.py
+++ scripts/dialogre_to_neo4j.py
@@ -1,10 +1,10 @@
-from src.paths import LOCAL_DATA_PATH
+from src.paths import LOCAL_RAW_DATA_PATH
 from src.processing import Neo4jGraph
 
 
 if __name__ == "__main__":
     graph = Neo4jGraph()
-    dialogues_path = LOCAL_DATA_PATH / 'dialog-re/data'
+    dialogues_path = LOCAL_RAW_DATA_PATH / 'dialog-re/data'
     json_files = ["train.json", "test.json", "dev.json"]
     graph.import_dialogre_data(dialogues_path, json_files)
     graph.close()
