b6dde7fa4df386f3f884284d95afe5955131b230
Murilo Bellatini <murilobell@gmail.com>
2023-08-15 14:20:40 +0000

    Extend class extractor

--- scripts/transformations/class_extractor.sh
+++ scripts/transformations/class_extractor.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+echo "Choose a scan mode:"
+echo "1) Full Scan (includes methods and decorators)"
+echo "2) Quick Scan (only class and function names)"
+read -p "Enter your choice (1/2): " choice
+
+echo "Press Enter to start..."
+read
+
+if [[ $choice -eq 1 ]]; then
+    pattern='^class |^def |^@|^[[:space:]]{4}def |^[[:space:]]{4}@'
+else
+    pattern='^class |^def '
+fi
+
 find . -name "*.py" -type f -print0 | while IFS= read -r -d $'\0' file; do
     echo "$file"
-    grep -E '^class |^def ' "$file"
+    grep -E "$pattern" "$file"
     echo
 done
+
+echo "Press Enter to end..."
+read
