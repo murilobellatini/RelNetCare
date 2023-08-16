b98ada88369e650440920e1a01d463812194a38e
Murilo Bellatini <murilobell@gmail.com>
2023-08-16 12:54:52 +0000

    Change var position

--- llms-fine-tuning/llama-lora-fine-tuning/03_deploy_fastchat_api.sh
+++ llms-fine-tuning/llama-lora-fine-tuning/03_deploy_fastchat_api.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# Dynamic variables based on certain conditions (e.g., model size)
+model_size="7B"
+dataset_name="dummy_en"
+
+# Base paths
+ROOT_DIR="/home/murilo/RelNetCare"
+MODEL_DIR="$ROOT_DIR/models"
+FINE_TUNED_MODEL_DIR="$MODEL_DIR/fine-tuned"
+
+# Construct the model directory path using the base and specific paths
+model_name="llama-$model_size-hf"
+lora_adaptor_name="$model_name-lora-adaptor/$dataset_name"
+output_dir="$FINE_TUNED_MODEL_DIR/$lora_adaptor_name"
 
 cleanup() {
     echo "Cleaning up..."
@@ -15,20 +28,6 @@ declare -a pids
 # Ask the user for input
 echo "Which server do you want to start? (1 for gradio, 2 for open_ai): "
 read choice
-
-# Dynamic variables based on certain conditions (e.g., model size)
-model_size="7B"
-dataset_name="dummy_en"
-
-# Base paths
-ROOT_DIR="/home/murilo/RelNetCare"
-MODEL_DIR="$ROOT_DIR/models"
-FINE_TUNED_MODEL_DIR="$MODEL_DIR/fine-tuned"
-
-# Construct the model directory path using the base and specific paths
-model_name="llama-$model_size-hf"
-lora_adaptor_name="$model_name-lora-adaptor/$dataset_name"
-output_dir="$FINE_TUNED_MODEL_DIR/$lora_adaptor_name"
 
 # Deploy FastChat API Wrapper
 python3 -m fastchat.serve.controller &
