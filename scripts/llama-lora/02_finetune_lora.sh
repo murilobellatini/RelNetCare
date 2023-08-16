db4b7428a25edb08358a3a31f87b7e186fbe6ca4
Murilo Bellatini <murilobell@gmail.com>
2023-08-16 16:02:18 +0000

    Improve pipeline

--- scripts/llama-lora/02_finetune_lora.sh
+++ scripts/llama-lora/02_finetune_lora.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Dynamic variables based on certain conditions (e.g., model size)
 model_size="7B"
-dataset_name="dialog-re-llama-train-dev"
+dataset_name="dialog-re-llama-11cls-train-dev"
 data_layer="processed/dialog-re-llama"
 
 # Base paths
@@ -10,7 +10,6 @@ LLAMA_LORA_DIR=$ROOT_DIR/llms-fine-tuning/llama-lora-fine-tuning
 MODEL_DIR="$ROOT_DIR/models"
 DATA_DIR="$ROOT_DIR/data/$data_layer"
 CUSTOM_MODEL_DIR="$MODEL_DIR/custom"
-FINE_TUNED_MODEL_DIR="$MODEL_DIR/fine-tuned"
 
 # Construct the model directory path using the base and specific paths
 model_name="llama-$model_size-hf"
@@ -18,7 +17,6 @@ hf_model_dir="$CUSTOM_MODEL_DIR/$model_name"
 data_path="$DATA_DIR/$dataset_name.json"
 lora_adaptor_name="$model_name-lora-adaptor/$dataset_name"
 lora_adaptor_dir="$CUSTOM_MODEL_DIR/$lora_adaptor_name"
-output_dir="$FINE_TUNED_MODEL_DIR/$lora_adaptor_name"
 
 # Train lora (for fine-tuning llama)
 deepspeed "$LLAMA_LORA_DIR/fastchat/train/train_lora.py" \
@@ -45,9 +43,3 @@ deepspeed "$LLAMA_LORA_DIR/fastchat/train/train_lora.py" \
     --model_max_length 512 \
     --gradient_checkpointing True \
     --lora_dropout 0.05
-
-# Merge lora adaptor with llama for fine-tuned behavior
-python -m fastchat.model.apply_lora \
-    --base "$hf_model_dir" \
-    --target "$output_dir" \
-    --lora "$lora_adaptor_dir"
