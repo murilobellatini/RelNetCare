f89d3213b652b99d8c8956b1e34a71e731d1d829
Murilo Bellatini <murilobell@gmail.com>
2023-08-21 17:03:13 +0000

    Create first script

--- scripts/transformations/transform_dialogre_llama.py
+++ scripts/transformations/transform_dialogre_llama.py
@@ -1,100 +1,55 @@
 import os
 import json
-
-def filter_skip_relations(all_relations, allowed_relations):
-    """Return a list of relations not in allowed_relations."""
-    return [r for r in all_relations if r not in allowed_relations]
+import argparse
 
 
-def transform_data_to_llm_format(data, preprompt="", start_idx=0, skip_relations=[], max_turns=10, max_speakers=2):
-    """Transform the given data into LLM ready dialogue format."""
-    new_data = []
-    identity_counter = start_idx
-
-    for conversation, triples in data:
-        # Filtering dialogues based on max_turns criteria
-        if max_turns is not None and len(conversation) > max_turns:
-            continue
-
-        # Extracting speaker names from each dialogue turn and checking based on max_speakers criteria
-        if max_speakers is not None:
-            speakers = set([turn.split(":")[0].strip() for turn in conversation])
-            if len(speakers) > max_speakers:
-                continue
-
-
-        triples_text = [
-            {
-                "x": triple["x"],
-                "x_type": triple["x_type"],
-                "r": triple["r"][0].split(':')[-1],
-                "y": triple["y"],
-                "y_type": triple["y_type"]
-            }
-            for triple in triples
-            if triple["r"] and triple["r"][0].split(':')[-1] not in skip_relations
-        ]
-
-        conversation_entry = {
-            "id": f"identity_{identity_counter}",
-            "conversations": [
-                {"from": "human", #tried: human
-                 "value": preprompt + "\n".join(conversation)},
-                {"from": "gpt", #tried: gpt
-                 "value": str(json.dumps(triples_text))}
-            ]
+class Config:
+    def __init__(self, max_turns=None, max_speakers=None):
+        
+        self.all_relations = {
+            "positive_impression", "negative_impression", "acquaintance", 
+            "alumni", "boss", "subordinate", "client", "dates", "friends", 
+            "girl/boyfriend", "neighbor", "roommate", "children", "other_family", 
+            "parents", "siblings", "spouse", "place_of_residence", "visited_place", 
+            "origin", "employee_or_member_of", "schools_attended", "works", "age", 
+            "date_of_birth", "major", "place_of_work", "title", "alternate_names", 
+            "pet", "residents_of_place", "visitors_of_place", "employees_or_members", 
+            "students", "unanswerable"
         }
-
-        identity_counter += 1
-
-        if triples_text:
-            new_data.append(conversation_entry)
-
-    return new_data
-
-
-def create_directory_if_not_exists(directory_path):
-    """Create a directory if it does not already exist."""
-    if not os.path.exists(directory_path):
-        os.makedirs(directory_path)
-
-
-def process_and_save_data(file_sets, input_dir, output_dir, skip_relations, preprompt="", total_relation_count=35, max_turns=10, max_speakers=2):
-    """Process the input data and save in the specified format."""
-    output_data = []
-    for files in file_sets:
-        data_folder_name = output_dir.split('/')[-1]
-        dataset_name = f"{data_folder_name}-{total_relation_count-len(skip_relations)}cls-{'-'.join(files)}"
-        last_data_idx = 0
-        new_format = []
-
-        create_directory_if_not_exists(output_dir)
-
-        for f in files:
-            input_data_path = os.path.join(input_dir, f'{f}.json')
-            
-            # Check if file exists
-            if os.path.exists(input_data_path):
-                with open(input_data_path, encoding='utf8') as fp:
-                    data = json.load(fp)
-
-                new_format.extend(transform_data_to_llm_format(
-                    data, preprompt=preprompt, start_idx=last_data_idx,
-                    skip_relations=skip_relations, max_turns=max_turns, max_speakers=max_speakers))
-                last_data_idx = len(new_format)
-
-        output_data_path = os.path.join(output_dir, f'{dataset_name}.json')
-        with open(output_data_path, 'w', encoding='utf8') as fp:
-            json.dump(new_format, fp)
-
-        print(files, len(new_format))
+        self.allowed_relations = {
+            "acquaintance", "children", "other_family", "parents", 
+            "siblings", "spouse", "place_of_residence", "visited_place", 
+            "pet", "residents_of_place", "visitors_of_place"
+            }
         
-        output_data.append(new_format)
+        self.skip_relations = self.filter_skip_relations()
+        self.total_relation_count = len(self.skip_relations)
+
+        self.ds_type = ""
+        self.ds_root = f"dialog-re-llama{self.ds_type}"
+        self.input_dir = "/home/murilo/RelNetCare/data/raw/dialog-re"
+        self.file_sets = [['train', 'dev'], ['test']]
+        self.max_turns = max_turns
+        self.max_speakers = max_speakers
+        self.output_dir = self.get_output_folder_name()
+        self.preprompt = self.generate_preprompt()
         
-    return output_data
+        print("output_dir=",self.output_dir)
+
+    def filter_skip_relations(self):
+        return [r for r in self.all_relations if r not in self.allowed_relations]
+
+    def get_output_folder_name(self):
+        parts = [self.ds_root, f"{len(self.allowed_relations)}cls"]
+        if self.max_turns:
+            parts.append(f"{self.max_turns}trn")
+        if self.max_speakers:
+            parts.append(f"{self.max_speakers}spkr")
+        return os.path.join("/home/murilo/RelNetCare/data/processed", "-".join(parts))
+
+    def generate_preprompt(self, add_one_shot=False):
         
-add_one_shot = False
-one_shot = """
+        one_shot = """
 Input:
 [
 "User: My daughter, Emma, recently moved to London.",
@@ -112,59 +67,139 @@ Output:
 ]
 """
 
-all_relations = {
-"positive_impression", "negative_impression", "acquaintance", 
-"alumni", "boss", "subordinate", "client", "dates", "friends", 
-"girl/boyfriend", "neighbor", "roommate", "children", "other_family", 
-"parents", "siblings", "spouse", "place_of_residence", "visited_place", 
-"origin", "employee_or_member_of", "schools_attended", "works", "age", 
-"date_of_birth", "major", "place_of_work", "title", "alternate_names", 
-"pet", "residents_of_place", "visitors_of_place", "employees_or_members", 
-"students", "unanswerable"
-}
-allowed_relations = {"acquaintance", "children", "other_family", "parents", 
-                    "siblings", "spouse", "place_of_residence", "visited_place", 
-                    "pet", "residents_of_place", "visitors_of_place"}
-
-# allowed_relations = all_relations  # uncomment to allow all relations!
-
-preprompt = f"""
+        preprompt = f"""
 Extract personal relevant entities, and their relations. Return only the jsonl format list .
 
 Ontology: 
-- relations: {str(allowed_relations).replace("'", '"')}
+- relations: {str(self.allowed_relations).replace("'", '"')}
 - types: {{"ORG", "GPE", "PERSON", "DATE", "EVENT", “ANIMAL”}}
 {one_shot if add_one_shot else ""}
 Input:
 """
-
-
-def get_output_folder_name(ds_root, skip_relations, max_turns=10, max_speakers=2):
-    """Generate a concise and descriptive output folder name."""
-    parts = [ds_root, f"{len(all_relations) - len(skip_relations)}cls"]
+        return preprompt
     
-    if max_turns:
-        parts.append(f"{max_turns}trn")
     
-    if max_speakers:
-        parts.append(f"{max_speakers}spkr")
+class DataTransformer:
+    @staticmethod
+    def transform_data_to_llm_format(data, config, start_idx=0):
+        """Transform the given data into LLM ready dialogue format."""
+        preprompt = config.preprompt
+        skip_relations = config.skip_relations
+        max_turns = config.max_turns
+        max_speakers = config.max_speakers
+        
+        new_data = []
+        identity_counter = start_idx
+
+        for conversation, triples in data:
+            # Filtering dialogues based on max_turns criteria
+            if max_turns is not None and len(conversation) > max_turns:
+                continue
+
+            # Extracting speaker names from each dialogue turn and checking based on max_speakers criteria
+            if max_speakers is not None:
+                speakers = set([turn.split(":")[0].strip() for turn in conversation])
+                if len(speakers) > max_speakers:
+                    continue
+
+
+            triples_text = [
+                {
+                    "x": triple["x"],
+                    "x_type": triple["x_type"],
+                    "r": triple["r"][0].split(':')[-1],
+                    "y": triple["y"],
+                    "y_type": triple["y_type"]
+                }
+                for triple in triples
+                if triple["r"] and triple["r"][0].split(':')[-1] not in skip_relations
+            ]
+
+            conversation_entry = {
+                "id": f"identity_{identity_counter}",
+                "conversations": [
+                    {"from": "human", #tried: human
+                    "value": preprompt + "\n".join(conversation)},
+                    {"from": "gpt", #tried: gpt
+                    "value": str(json.dumps(triples_text))}
+                ]
+            }
+
+            identity_counter += 1
+
+            if triples_text:
+                new_data.append(conversation_entry)
+
+        return new_data
+
+    @staticmethod
+    def process_and_save_data(config):
+        """Process the input data and save in the specified format."""
+        file_sets=config.file_sets
+        input_dir=config.input_dir
+        output_dir=config.output_dir
+        skip_relations=config.skip_relations
+        preprompt=config.preprompt
+        total_relation_count=config.total_relation_count
+        max_turns=config.max_turns
+        max_speakers=config.max_speakers
+
+        output_data = []
+        for files in file_sets:
+            data_folder_name = output_dir.split('/')[-1]
+            dataset_name = f"{data_folder_name}-{'-'.join(files)}"
+            last_data_idx = 0
+            new_format = []
+
+            DataManager.create_directory_if_not_exists(output_dir)
+
+            for f in files:
+                input_data_path = os.path.join(input_dir, f'{f}.json')
+                
+                # Check if file exists
+                if os.path.exists(input_data_path):
+                    with open(input_data_path, encoding='utf8') as fp:
+                        data = json.load(fp)
+
+                    new_format.extend(DataTransformer().transform_data_to_llm_format(data, config, last_data_idx))
+                    last_data_idx = len(new_format)
+
+            output_data_path = os.path.join(output_dir, f'{dataset_name}.json')
+            with open(output_data_path, 'w', encoding='utf8') as fp:
+                json.dump(new_format, fp)
+
+            print(files, len(new_format))
+            
+            output_data.append(new_format)
+            
+        return output_data
+
+class DataManager:
+    @staticmethod
+    def create_directory_if_not_exists(directory_path):
+        """Create a directory if it does not already exist."""
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+            
+    @staticmethod
+    def read_json_file(file_path):
+        with open(file_path, encoding='utf8') as fp:
+            return json.load(fp)
+
+    @staticmethod
+    def write_json_file(data, file_path):
+        with open(file_path, 'w', encoding='utf8') as fp:
+            json.dump(data, fp)
+
     
-    return "-".join(parts)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Transform data to a desired format.")
+    parser.add_argument("--max_turns", type=int, default=None, help="Maximum number of turns.")
+    parser.add_argument("--max_speakers", type=int, default=None, help="Maximum number of speakers.")
+    args = parser.parse_args()
 
-skip_relations = filter_skip_relations(all_relations, allowed_relations)
+    # Create a Config instance with the parsed arguments
+    config = Config(args.max_turns, args.max_speakers)
 
-# Directories
-ds_type = ""
-# ds_type = "-typed-pp"
-max_turns=10
-max_speakers=2
-ds_root = f"dialog-re-llama{ds_type}"
-INPUT_DIR = "/home/murilo/RelNetCare/data/raw/dialog-re"
-OUTPUT_DIR = os.path.join("/home/murilo/RelNetCare/data/processed", get_output_folder_name(
-    ds_root=ds_root, skip_relations=skip_relations, max_turns=max_turns, max_speakers=max_speakers))
-
-FILE_SETS = [['train', 'dev'], ['test']]
-
-print(preprompt)
-
-data = process_and_save_data(FILE_SETS, INPUT_DIR, OUTPUT_DIR, skip_relations, preprompt, max_turns=max_turns, max_speakers=max_speakers)
+    # Process and save data
+    DataTransformer.process_and_save_data(config)
